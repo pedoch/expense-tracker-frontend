c1b9a948d384d684be1c035874c8efa146268b35
PedroOgheneochuko <pedroogheneochuko@gmail.com>
2020-04-30 00:49:37 +0100

    Updates

---
+++ precache-manifest.faf884a49824cdc9ffebbf559105d19d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43c425031ba75d9a506502b6c93870b",
+    "url": "/expense-tracker-frontend/index.html"
+  },
+  {
+    "revision": "56180ef9281891075ae1",
+    "url": "/expense-tracker-frontend/static/css/main.1ad7d555.chunk.css"
+  },
+  {
+    "revision": "1cbd70ed7df4b064f663",
+    "url": "/expense-tracker-frontend/static/js/2.fea320c8.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/expense-tracker-frontend/static/js/2.fea320c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56180ef9281891075ae1",
+    "url": "/expense-tracker-frontend/static/js/main.2e22b494.chunk.js"
+  },
+  {
+    "revision": "b7531b8c535301411029",
+    "url": "/expense-tracker-frontend/static/js/runtime-main.31c61db2.js"
+  }
+]);
